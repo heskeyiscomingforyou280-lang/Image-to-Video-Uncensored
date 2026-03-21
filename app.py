0a584b1b8398dc72d77a1bbf9145ac4d6a3d1c4f
heskeyiscomingforyou280-lang <heskeyiscomingforyou280@gmail.com>
2026-03-22 01:08:47 +0400

    Update app.py

--- app.py
+++ app.py
@@ -122,8 +122,8 @@ pipe = WanImageToVideoPipeline.from_pretrained(
 # LOAD LORA ADAPTERS
 # =========================================================
 pipe.load_lora_weights(
-    "heskeyiscoming/spit",
-    weight_name="wan22-spitonanother-54epoc-i2v-high.safetensors",
+    "heskeyiscoming/liftleg",
+    weight_name="highE9AB98E68AACE885.97hK.safetensors",
     adapter_name="i2v_scat"
 )
 pipe.load_lora_weights(
@@ -133,8 +133,8 @@ pipe.load_lora_weights(
 )
 
 pipe.load_lora_weights(
-    "heskeyiscoming/spit",
-    weight_name="wan22-spitonanother-55epoc-i2v-low.safetensors",
+    "heskeyiscoming/liftleg",
+    weight_name="lowE9AB98E68AACE885.E9ax.safetensors",
     adapter_name="i2v_scat_2",
     load_into_transformer_2=True
 )
